6c0f47930a728b4d79148f14f1a8d15c0aa2370f
CoopTRUE <coopdog4545@gmail.com>
2023-08-29 18:40:38 -0700

    Formatted Code

--- main.py
+++ main.py
@@ -7,24 +7,25 @@ import matplotlib.pyplot as plt
 def insertion_sort(arr: list[int]):
     for i in range(1, len(arr)):
         j = i - 1
-        while j >= 0 and arr[j] > arr[j+1]:
-            arr[j], arr[j+1] = arr[j+1], arr[j]
+        while j >= 0 and arr[j] > arr[j + 1]:
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
             j -= 1
 
 
 def selection_sort(arr: list[int]):
     for i in range(len(arr)):
         min_idx = i
-        for j in range(i+1, len(arr)):
+        for j in range(i + 1, len(arr)):
             if arr[j] < arr[min_idx]:
                 min_idx = j
         arr[min_idx], arr[i] = arr[i], arr[min_idx]
 
+
 def bubble_sort(arr: list[int]):
     for i in range(len(arr)):
-        for j in range(len(arr)-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+        for j in range(len(arr) - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
 
 def quick_sort(arr: list[int]):
